c0239a26b1be370b5d5708137eb8a9364f002c09
Anarion <anarion80@gmail.com>
2023-09-27 19:23:49 +0200

    Update Kibot

--- gen_kibot.sh
+++ gen_kibot.sh
@@ -2,12 +2,12 @@
 
 container_cmd=docker
 container_args="-w /board -v $(pwd):/board -v /tmp/.X11-unix:/tmp/.X11-unix --rm"
-docker_image="ghcr.io/inti-cmnb/kicad_auto_full"
-docker_tag="dev_k7"
+docker_image="ghcr.io/inti-cmnb/kicad7_auto_full"
+docker_tag="latest"
 
 for d in ./output/*; do
     if [ -d $d ]; then
-        for f in $d/pcbs/*; do
+        for f in $d/pcbs/*.kicad_pcb; do
             filename=${f##*/}
             file=${filename%.*}
             ${container_cmd} run ${container_args} ${docker_image}:${docker_tag} /bin/bash -c "cd $d/pcbs; kibot -b $filename -c /board/config.kibot.yaml"
